f09ecfb2e2a4f5fc1d11f11269d897b2e424201f
unknown <joderlan.silva@gmail.com>
2022-01-26 16:31:54 -0300

    finalizado templates home e contacts

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,37 +1,37 @@
 import React from "react";
 import Right from "../components/containerRight";
-import background from '../images/background.jpg'
 
+import background from '../images/background.jpg'
 import './styles.css'
-export default function Projects() {
-    return (
+export default function Projects(){
+    return(
 
         <div className="container">
-            <div className="project--left">
-                <section className="project" style={{
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'certer',
-                    backgroundImage: `url(${background})`,
+        <div className="contact--left">
+            <section className="contact" style={{
+                backgroundSize: 'cover',
+                backgroundPosition: 'certer',
+                backgroundImage: `url(${background})`
 
-                }}>
+            }}>
 
-                    <div class="project--vetical">
-                        <div class="project--horizontal">
-                            <div className="project--name">Projetos</div>
-                            <div class="project--description">
-
+                <div class="contact--vetical">
+                    <div class="contact--horizontal">
+                        <div class="contact--description">
+                            <div class="contact--contacts">
                             </div>
-
                         </div>
 
-                        <div className="project--conteudo" >
-                            conteudo aqui
-                        </div>
+
                     </div>
-                </section>
-            </div>
-            <Right />
+                </div>
+
+
+            </section>
         </div>
+        <Right />
+    </div>
+
 
     )
 }
